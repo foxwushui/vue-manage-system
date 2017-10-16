fafe95ec47e5bb6d587cb3b8485a343c922e1d11
foxwushui <foxwushui@163.com>
2017-10-16 17:39:14 +0800

    c

--- src/components/common/Home.vue
+++ src/components/common/Home.vue
@@ -3,7 +3,7 @@
         <v-head></v-head>
         <v-sidebar></v-sidebar>
         <div class="content">
-            <transition name="move" mode="out-in"><router-view></router-view>dasdads</transition>
+            <transition name="move" mode="out-in"><router-view></router-view>dasasdasdasadasdasda</transition>
         </div>
     </div>
 </template>
